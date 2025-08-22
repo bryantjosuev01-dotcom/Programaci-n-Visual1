4f41e0d74741f16d94018be1cd86e787c78d5654
bryantjosuev01-dotcom <bryantjosuev28@gmail.com>
2025-08-22 04:10:07 +0200

    Add files via upload

---
+++ Form1_clics.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contador_clics
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int contador = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            contador++;
+            contador_b.Text = $"Clics: {contador}";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
